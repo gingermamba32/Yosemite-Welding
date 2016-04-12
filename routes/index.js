702bfd8181ffe1b684b4794beb88f00c7ff534b3
Michael_Montero <mmontero2012@gmail.com>
2016-04-11 18:53:00 -0600

    chnages

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ process.env.PWD = process.cwd()
 /* GET home page. */
 var html_dir = '../views/';
 router.get('/', function(req, res, next) {
-	//res.sendFile(path.join(process.env.PWD+'/index.html'));
-  res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
+	res.sendFile(path.join(process.env.PWD+'/index.html'));
+  //res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
 });
 
 module.exports = router;
