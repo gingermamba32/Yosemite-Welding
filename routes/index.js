5b9e3198331e0585828a327b588f6c324933e093
Michael_Montero <mmontero2012@gmail.com>
2016-04-16 14:26:31 -0600

    changes

---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var nodemailer = require('nodemailer');
+
+var user = require('./password.js').email;
+var password = require('./password.js').pass;
+// heroku statuc file server
+process.env.PWD = process.cwd()
+
+/* GET home page. */
+var html_dir = '../views/';
+router.get('/', function(req, res, next) {
+	res.sendFile(path.join(process.env.PWD+'/index.html'));
+  //res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
+});
+
+router.get('/about-us', function(req, res, next) {
+	res.sendFile(path.join(process.env.PWD+'/about-us.html'));
+  //res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
+});
+router.get('/contact-us', function(req, res, next) {
+	res.sendFile(path.join(process.env.PWD+'/contact-us.html'));
+  //res.sendFile('/Users/michaelmontero/Desktop/StoreFrontTemplate/views/index.html');
+});
+
+// Set up the sender......
+
+
+
+// Create a new email account to send these correctly
+
+
+router.post('/send', function(req, res, next){
+	console.log(req.body.email);
+
+	var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: user, // Your email id
+            pass: password // Your password
+        }
+    });
+
+	var mailOptions = {
+    from: req.body.email, // sender address
+    to: 'michaelm@beautyindustrygroup.com', // list of receivers
+    subject: 'Re: Contact Us -> ' + req.body.name, // Subject line
+    text: req.body.message
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        res.json({yo: 'error'});
+    } else{
+        console.log('Message sent: ' + info.response);
+        res.json({yo: info.response});
+    	};
+	});
+
+
+
+
+})
+
+module.exports = router;
